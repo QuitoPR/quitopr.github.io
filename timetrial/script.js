4004375f02fa82f29e521e1fc0026dec2eac7868
QuitoPR <quito.pr@hotmail.com>
2023-11-05 11:01:54 +0100

    fixed output

--- timetrial/script.js
+++ timetrial/script.js
@@ -9,20 +9,45 @@ function generateRandomList() {
     });
 
     const values = {
-        Location: ["Monte-Carlo", "Sweden", "Mexico", "Croatia", "Portugal", "Sardegna", "Kenya", "Estonia", "Finland", "Greece", "Chile", "Japan", "Mediterraneo", "Pacifico", "Oceania", "Scandia", "Iberia"],
-        Stage: Array.from({ length: 12 }, (_, i) => (i + 1).toString()),
+        Location: ["Monte-Carlo", "Sweden", /* Add more location values here */],
         Weather: ["Dry", "Wet"],
-        Car: ["WRC", "WRC2", "JWRC", "WRC '17-21", "WRC '97-11", "Rally2", "Rally4", "NR4/R4", "S2000", "S1600", "F2 Kit", "Group A", "Group B 4WD", "Group B RWD", "H3 RWD", "H2 RWD", "H2 FWD", "H1 FWD"]
-        
+        Car: ["WRC", "WRC2", "JWRC", "WRC '17-21", "WRC '97-11", /* Add more car values here */]
+    };
+
+    const randomValues = {
+        Location: () => values.Location[Math.floor(Math.random() * values.Location.length)],
+        Weather: () => values.Weather[Math.floor(Math.random() * values.Weather.length)],
+        Car: () => values.Car[Math.floor(Math.random() * values.Car.length)],
     };
 
     let resultList = [];
 
     selectedCategories.forEach((category) => {
-        const randomValue = values[category][Math.floor(Math.random() * values[category].length)];
-        resultList.push(category + ' ' + randomValue);
+        if (category !== "Stage") {
+            resultList.push(randomValues[category]());
+        }
     });
+
+    // Generate a Stage value
+    if (selectedCategories.includes("Stage")) {
+        const stageValue = "Stage " + (Math.floor(Math.random() * 12) + 1);
+        resultList.push(stageValue);
+    }
 
     const resultElement = document.getElementById('result');
     resultElement.textContent = resultList.join(', ');
 }
+
+// Show/hide value checkboxes when category checkboxes change
+document.getElementsByName('category').forEach((categoryCheckbox) => {
+    categoryCheckbox.addEventListener('change', function () {
+        const valuesDiv = categoryCheckbox.parentElement.querySelector('.values');
+        valuesDiv.style.display = categoryCheckbox.checked ? 'block' : 'none';
+        
+        // Check/uncheck related value checkboxes based on the category checkbox
+        const valueCheckboxes = valuesDiv.querySelectorAll('input[name="values"]');
+        valueCheckboxes.forEach((valueCheckbox) => {
+            valueCheckbox.checked = categoryCheckbox.checked;
+        });
+    });
+});
