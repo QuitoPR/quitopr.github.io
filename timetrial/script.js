81216f1781dcd4ed5c5e432f095bf8030f1e1a5d
QuitoPR <quito.pr@hotmail.com>
2023-11-05 10:51:08 +0100

    proper categories

--- timetrial/script.js
+++ timetrial/script.js
@@ -9,8 +9,11 @@ function generateRandomList() {
     });
 
     const values = {
-        Stage: ["1", "2", "3"],
-        Location: ["Rally Iberia", "Portugal", "Germany"]
+        Location: ["Monte-Carlo", "Sweden", "Mexico", "Croatia", "Portugal", "Sardegna", "Kenya", "Estonia", "Finland", "Greece", "Chile", "Japan", "Mediterraneo", "Pacifico", "Oceania", "Scandia", "Iberia"],
+        Stage: Array.from({ length: 12 }, (_, i) => (i + 1).toString()),
+        Weather: ["Dry", "Wet"],
+        Car: ["WRC", "WRC2", "JWRC", "WRC '17-21", "WRC '97-11", "Rally2", "Rally4", "NR4/R4", "S2000", "S1600", "F2 Kit", "Group A", "Group B 4WD", "Group B RWD", "H3 RWD", "H2 RWD", "H2 FWD", "H1 FWD"]
+        
     };
 
     let resultList = [];
